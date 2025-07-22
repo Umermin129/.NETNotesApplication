21ce49690de907d4ae2aaad49286730e9daad394
Umer Minhas <umermin129@gmail.com>
2025-07-22 05:36:42 -0700

    Add RegistrationViewModel

--- UseNotesApplication/ViewModels/RegistrationViewModel.cs
+++ UseNotesApplication/ViewModels/RegistrationViewModel.cs
@@ -7,5 +7,19 @@ namespace UseNotesApplication.ViewModels
         [Required]
         [MaxLength(50)]
         public string UserName { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Please upload exactly 5 images.")]
+        public List<IFormFile> Images { get; set; }
+
+        [Required(ErrorMessage = "Provide a sequence number for each image.")]
+        public List<int> Sequence { get; set; }
+
     }
 }
