15072b5b011563cce83cd865b4e16ef64e20a9e0
Umermin129 <115006215+Umermin129@users.noreply.github.com>
2025-07-28 11:56:26 +0500

    Added Loading Notes data Logic

--- UseNotesApplication/Controllers/HomeController.cs
+++ UseNotesApplication/Controllers/HomeController.cs
@@ -1,21 +1,45 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using UseNotesApplication.Data;
 using UseNotesApplication.Models;
+using UseNotesApplication.ViewModels;
 
 namespace UseNotesApplication.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly AppDbContext _context;
+        public HomeController(ILogger<HomeController> logger,AppDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var UserName = HttpContext.Session.GetString("UserName"); 
+            var user  = _context.Users.Include(u=>u.Notes).FirstOrDefault(u=>u.UserName == UserName);
+
+            if(user == null) {
+                return RedirectToAction("Login","Account");
+                }
+
+            var model = new HomeViewModel
+            {
+                UserName = user.UserName,
+                Name = user.Name,
+                email = user.Email,
+                TaskLists = user.Notes.Where(u => !u.IsDeleted).Select(n => new TaskEdit
+                {
+                    Title = n.Title,
+                    Description = n.Description,
+                    Status = n.Status,
+                    LastUpdated = n.LastModifiedAt,
+                }).ToList()
+            };
+            return View(model);
         }
 
         public IActionResult Privacy()
